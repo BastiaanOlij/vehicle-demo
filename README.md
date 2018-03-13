51ffbc43b339cd4b751a5ceeb8e36af68287e758
Bastiaan Olij <mux213@gmail.com>
2018-03-13 18:21:50 +1100

    First commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Godot VehicleBody Demo project
+
+This is a Godot demo project in support of my vehicle body youtube tutorial videos.
+
+It has been tested with Godot 3.0.2 stable release.
+You will need to download this in order to run this project.
+
+### License
+
+Source code is released under MIT (see LICENSE).
+Note that this project contains textures and models owned by their authors.
+See links below for exact licenses.
+
+### Useful links
+
+ - [Main website](https://godotengine.org)
+ - [Source code](https://github.com/godotengine/godot)
+ - [Documentation](http://docs.godotengine.org)
+ - [Community hub](https://godotengine.org/community)
+ - [My Youtube channel](https://www.youtube.com/channel/UCrbLJYzJjDf2p-vJC011lYw)
+ - [HDRIHaven](https://hdrihaven.com/)
+ - [Textures.com](https://www.textures.com/)
+ - [Blender GLTF export](https://github.com/KhronosGroup/glTF-Blender-Exporter)
+ - [Turbosquid caterham](https://www.turbosquid.com/3d-models/free-max-mode-caterham-sports-car/733021)
+
---
+++ Script/Car.gd
@@ -0,0 +1,58 @@
+extends VehicleBody
+
+############################################################
+# behaviour values
+
+export var MAX_ENGINE_FORCE = 200.0
+export var MAX_BRAKE_FORCE = 5.0
+export var MAX_STEER_ANGLE = 0.5
+
+export var steer_speed = 5.0
+
+var steer_target = 0.0
+var steer_angle = 0.0
+
+############################################################
+# Input
+
+export var joy_steering = JOY_ANALOG_LX
+export var steering_mult = -1.0
+export var joy_throttle = JOY_ANALOG_R2
+export var throttle_mult = 1.0
+export var joy_brake = JOY_ANALOG_L2
+export var brake_mult = 1.0
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _physics_process(delta):
+	var steer_val = steering_mult * Input.get_joy_axis(0, joy_steering)
+	var throttle_val = throttle_mult * Input.get_joy_axis(0, joy_throttle)
+	var brake_val = brake_mult * Input.get_joy_axis(0, joy_brake)
+	
+	# overrules for keyboard
+	if Input.is_action_pressed("ui_up"):
+		throttle_val = 1.0
+	if Input.is_action_pressed("ui_down"):
+		brake_val = 1.0
+	if Input.is_action_pressed("ui_left"):
+		steer_val = 1.0
+	elif Input.is_action_pressed("ui_right"):
+		steer_val = -1.0
+	
+	engine_force = throttle_val * MAX_ENGINE_FORCE
+	brake = brake_val * MAX_BRAKE_FORCE
+	
+	steer_target = steer_val * MAX_STEER_ANGLE
+	if (steer_target < steer_angle):
+		steer_angle -= steer_speed * delta
+		if (steer_target > steer_angle):
+			steer_angle = steer_target
+	elif (steer_target > steer_angle):
+		steer_angle += steer_speed * delta
+		if (steer_target < steer_angle):
+			steer_angle = steer_target
+	
+	steering = steer_angle
