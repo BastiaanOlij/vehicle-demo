40445614bd9a5959945846433f1fb207f61fd077
Bastiaan Olij <mux213@gmail.com>
2018-09-06 19:32:52 +1000

    Part 4 - Adjusting the track size

--- Assets/Scenes/track-base.gd
+++ Assets/Scenes/track-base.gd
@@ -2,6 +2,7 @@ tool
 extends Path
 
 export var track_width = 8.0 setget set_track_width, get_track_width
+export var lower_ground_width = 12.0 setget set_lower_ground_width, get_lower_ground_width
 export var rail_distance = 1.0 setget set_rail_distance, get_rail_distance
 export var post_distance = 1.0 setget set_post_distance, get_post_distance
 
@@ -15,6 +16,15 @@ func set_track_width(new_width):
 
 func get_track_width():
 	return track_width
+
+func set_lower_ground_width(new_width):
+	if lower_ground_width != new_width:
+		lower_ground_width = new_width
+		is_dirty = true
+		call_deferred("_update")
+
+func get_lower_ground_width():
+	return lower_ground_width
 
 func set_rail_distance(new_dist):
 	if rail_distance != new_dist:
@@ -41,6 +51,58 @@ func _update():
 	# how long is our track?
 	var curve_length = curve.get_baked_length()
 	
+	###################################################################################
+	# update our track
+	var track_half_width = track_width * 0.5
+	var ground_half_width = lower_ground_width * 0.5
+	
+	var track = $Road.polygon
+	track.set(0, Vector2(-track_half_width, 0.0))
+	track.set(1, Vector2(-track_half_width, -0.1))
+	track.set(2, Vector2( track_half_width, -0.1))
+	track.set(3, Vector2( track_half_width, 0.0))
+	$Road.polygon = track
+	
+	var ground = $Ground.polygon
+	ground.set(1, Vector2( track_half_width + 2.0, -0.1))
+	ground.set(0, Vector2(-track_half_width - 2.0, -0.1))
+	ground.set(2, Vector2( lower_ground_width, -4.01))
+	ground.set(3, Vector2( lower_ground_width + 0.1, -4.1))
+	ground.set(4, Vector2(-lower_ground_width - 0.1, -4.1))
+	ground.set(5, Vector2(-lower_ground_width, -4.0))
+	$Ground.polygon = ground
+	
+	###################################################################################
+	# update our rails
+	var rail_position = track_half_width + rail_distance
+	
+	var rail = $"Rail-L".polygon
+	rail.set(0, Vector2(rail_position, 0.5))
+	rail.set(1, Vector2(rail_position - 0.05, 0.47))
+	rail.set(2, Vector2(rail_position - 0.05, 0.43))
+	rail.set(3, Vector2(rail_position, 0.4))
+	rail.set(4, Vector2(rail_position, 0.55))
+	rail.set(5, Vector2(rail_position - 0.05, 0.32))
+	rail.set(6, Vector2(rail_position - 0.05, 0.28))
+	rail.set(7, Vector2(rail_position, 0.25))
+	rail.set(8, Vector2(rail_position + 0.05, 0.25))
+	rail.set(9, Vector2(rail_position + 0.05, 0.5))
+	$"Rail-L".polygon = rail
+	
+	rail = $"Rail-R".polygon
+	rail.set(0, Vector2(-rail_position, 0.5))
+	rail.set(1, Vector2(-rail_position + 0.05, 0.47))
+	rail.set(2, Vector2(-rail_position + 0.05, 0.43))
+	rail.set(3, Vector2(-rail_position, 0.4))
+	rail.set(4, Vector2(-rail_position, 0.55))
+	rail.set(5, Vector2(-rail_position + 0.05, 0.32))
+	rail.set(6, Vector2(-rail_position + 0.05, 0.28))
+	rail.set(7, Vector2(-rail_position, 0.25))
+	rail.set(8, Vector2(-rail_position - 0.05, 0.25))
+	rail.set(9, Vector2(-rail_position - 0.05, 0.5))
+	$"Rail-R".polygon = rail
+	
+	###################################################################################
 	# update our posts
 	var post_count = floor(curve_length / post_distance)
 	var real_post_dist = curve_length / post_count
@@ -61,7 +123,7 @@ func _update():
 		xf.basis.x = lookat.cross(up).normalized()
 		xf.basis.y = xf.basis.x.cross(lookat).normalized()
 		
-		var v = Vector3(track_width * 0.5 + rail_distance, 0.0, 0.0)
+		var v = Vector3(rail_position, 0.0, 0.0)
 		
 		t.basis = Basis()
 		t.origin = xf.xform(-v)
@@ -70,6 +132,21 @@ func _update():
 		t.basis = Basis()
 		t.origin = xf.xform(v)
 		$Posts.multimesh.set_instance_transform(post_count + i, t)
+	
+	###################################################################################
+	# update our collision
+	
+	var collision = $CollisionShape.polygon
+	collision.set(0, Vector2(-rail_position, 0.0))
+	collision.set(1, Vector2( rail_position, 0.0))
+	collision.set(2, Vector2( rail_position, 5.0))
+	collision.set(3, Vector2( rail_position + 3.0, 5.0))
+	collision.set(4, Vector2( rail_position + 3.0, -1.0))
+	collision.set(5, Vector2(-rail_position - 3.0, -1.0))
+	collision.set(6, Vector2(-rail_position - 3.0, 5.0))
+	collision.set(7, Vector2(-rail_position, 5.0))
+	$CollisionShape.polygon=collision
+	
 		
 	is_dirty = false
 
